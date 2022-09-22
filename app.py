56ae8b0072ad90f687465366f87ed2cc175c4b90
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-09-22 17:01:38 -0600

    Fixed the bug in the filter_users_subscribed

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-from msilib import _directories
-
-
 list_of_strings = ['gabriel', 'natalia', 'chicago']
 
 for name in list_of_strings:
@@ -15,20 +12,24 @@ def print_string(name):
 for name in list_of_strings:
     print_string(name)
 
-list_of_users = [{
-    "username": "gabriel",
-    "is_subscribed": True,
-    "age": 26
-},
-{
-    "username": "natalia",
-    "is_subscribed": False,
-    "age": 37
-}]
+list_of_users = [
+    {
+        "username": "gabriel",
+        "is_subscribed": True,
+        "age": 26
+    },
+    {
+        "username": "natalia",
+        "is_subscribed": False,
+        "age": 37
+    }
+]
 
-def list_of_dictionary(list):
-    for user in list:
-        if(user["is_subscribed"] == False):
-            list.pop("user")
 
-    return list
+def filter_users_subscribed(users):
+    subscribed_users = []
+    for user in users:
+        if (user["is_subscribed"] == True):
+            subscribed_users.append(user)
+
+    return subscribed_users
